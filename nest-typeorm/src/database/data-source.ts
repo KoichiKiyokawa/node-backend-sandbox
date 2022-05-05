2f9859b65f808ea06d663a7410990a1c38a3bbce
Koichi Kiyokawa <koichi20110068@gmail.com>
2022-05-05 23:56:14 +0900

    chore: add NOTE

--- nest-typeorm/src/database/data-source.ts
+++ nest-typeorm/src/database/data-source.ts
@@ -3,5 +3,6 @@ import { DataSource } from 'typeorm';
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: 'dev.db',
+  // NOTE: ts-nodeで実行される場合は、`src/**/*entity.ts`が読み込まれるが、ランタイムでは、`dist/**/*entity.js`が読み込まれることに注意
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
 });
