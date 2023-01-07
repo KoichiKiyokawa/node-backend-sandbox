7a6964ab5c2087251fa6fafebe1cd5f7c6d4bc6a
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-01-07 22:04:51 +0900

    chore: ignore db log branch

--- feature-driven-template/src/lib/db.ts
+++ feature-driven-template/src/lib/db.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
+  /* c8 ignore next */
   log: process.env.NODE_ENV === "develop" ? ["query", "error", "info", "warn"] : [],
 });
