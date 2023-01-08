06ae59157d2ffe674223d21cfbf58289ed83fe67
Koichi Kiyokawa <koichi20110068@gmail.com>
2023-01-08 19:10:15 +0900

    chore: move ts-wire to dependent repo

--- ts-wire/demo/src/wire-generated.ts
+++
@@ -1,7 +0,0 @@
-import { FooRepository, db, BarRepository, FooService, FooController } from "./foo";
-
-const fooRepository = new FooRepository(db);
-const barRepository = new BarRepository(db);
-const fooService = new FooService(fooRepository, barRepository);
-const fooController = new FooController(fooService);
-export const leaves = [fooController];
--- ts-wire/src/utils.ts
+++
@@ -1,3 +0,0 @@
-export function camelize(str: string) {
-  return str[0].toLowerCase() + str.slice(1)
-}
